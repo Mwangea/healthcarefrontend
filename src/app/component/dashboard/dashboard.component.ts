4f0739e888de2c4bdac01c1950f112efafcf3bd8
mwangea <mwangeamusa@gmail.com>
2024-07-22 22:58:26 +0300

    dashboard

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { doctor, medicine } from './../../_model/user.model';
+import { Component, OnInit } from '@angular/core';
+import { appointment } from '../../_model/user.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  appointment: appointment[] = [];
+  doctors: doctor[]=[];
+  medicine: medicine[]=[];
+  activityOverView= {
+    appointments: 0,
+    newPatients: 0,
+    medicinesSold: 0,
+    labTests: 0
+  }
 
 }
+
